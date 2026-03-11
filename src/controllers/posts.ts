1e439f5dbb7eff27833837f304d0e309d2d8f59a
clizzix <godsilla1993@gmail.com>
2026-03-11 18:45:05 +0100

    applied full CRUD logic in express.js

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -18,3 +18,40 @@ export const getPosts: RequestHandler = async (req, res) => {
         res.status(500).json({ error: 'failed to fetch posts' });
     }
 };
+
+export const getPostsById: RequestHandler = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+        return res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'failed to fetch post' });
+    }
+};
+
+export const updatePostById: RequestHandler = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+            returnDocument: 'after',
+            runValidators: true,
+        });
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+        return res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'failed to update post' });
+    }
+};
+
+export const deletePostById: RequestHandler = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+        return res.json({
+            message: `Successfully deleted post with the Id: ${post.id}`,
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'failed to delete post' });
+    }
+};
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-import { createPost, getPosts } from '#controllers';
+import {
+    createPost,
+    getPosts,
+    getPostsById,
+    deletePostById,
+    updatePostById,
+} from '#controllers';
 
 const postRoutes = Router();
 
 postRoutes.post('/', createPost);
 postRoutes.get('/', getPosts);
+postRoutes.get('/:id', getPostsById);
+postRoutes.delete('/:id', deletePostById);
+postRoutes.put('/:id', updatePostById);
 
 export default postRoutes;
